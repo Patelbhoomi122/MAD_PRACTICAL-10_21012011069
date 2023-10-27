fb7b7b2f14a4845c5a316a29f63fbdaefe0fd79b
hp <patelbhoomi3203@gmail.com>
2023-10-27 15:08:56 +0530

    practical_10

--- app/src/main/java/com/example/mad_practical_10_21012011069/Person.kt
+++ app/src/main/java/com/example/mad_practical_10_21012011069/Person.kt
@@ -1,5 +1,6 @@
 package com.example.mad_practical_10_21012011069
 
+import org.json.JSONObject
 import java.io.Serializable
 
 class Person (
@@ -10,6 +11,18 @@ class Person (
     var address: String,
     var latitude:Double,
     var longitude:Double):Serializable{
-
+    constructor(jsonObject: JSONObject):this("","","","","",0.0,0.0) {
+        id = jsonObject.getString("id")
+        emailid = jsonObject.getString("email")
+        phoneNo = jsonObject.getString("phone")
+        val profileJson = jsonObject.getJSONObject("profile")
+        name = profileJson.getString("name")
+        address = profileJson.getString("address")
+        val locationJson = profileJson.getJSONObject("location")
+        latitude = locationJson.getDouble("lat")
+        longitude = locationJson.getDouble("long")
     }
+
+
+}
 
--- app/src/main/java/com/example/mad_practical_10_21012011069/PersonAdapter.kt
+++ app/src/main/java/com/example/mad_practical_10_21012011069/PersonAdapter.kt
@@ -1,15 +1,26 @@
 package com.example.mad_practical_10_21012011069
 
 import android.content.Context
+import android.content.Intent
 import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.google.android.material.button.MaterialButton
 
 class PersonAdapter(context:Context, val PersonArray :ArrayList<Person>):ArrayAdapter<Person>(context,0,PersonArray) {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view = LayoutInflater.from(context).inflate(R.layout.person_item,parent,false)
+
+        view.findViewById<TextView>(R.id.textView_name).text=PersonArray[position].name
+        view.findViewById<TextView>(R.id.textView_no).text=PersonArray[position].phoneNo
+        view.findViewById<TextView>(R.id.textView_email).text=PersonArray[position].emailid
+        view.findViewById<TextView>(R.id.textView_address).text=PersonArray[position].address
+        view.findViewById<MaterialButton>(R.id.button).setOnClickListener {
+            Intent(context,MapsActivity::class.java).putExtra("object",PersonArray[position]).apply { context.startActivity(this) }
+        }
         return view
     }
 
